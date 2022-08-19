715171f87725e8c1c1ab362626ea642519c4d26b
matt-townshend <hgq6521@autuni.ac.nz>
2022-08-19 14:35:15 +1200

    added react router for dynamic pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
@@ -11,12 +17,15 @@ const App = () =>  {
         <div>
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+
             </ul>
           <div className="content">
-            
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
           </div>
         </div>
         </Router>
